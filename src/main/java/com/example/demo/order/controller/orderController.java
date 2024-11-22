320817322aad70080c7b81624a2a26758d9c76dc
장동하 <alja34@naver.com>
2024-11-22 11:54:26 +0900

    update product

--- src/main/java/com/example/demo/order/controller/orderController.java
+++ src/main/java/com/example/demo/order/controller/orderController.java
@@ -1,41 +1,41 @@
-package com.example.demo.order.controller;
-
-//POJO : Plain Old Java Object
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class orderController {
-
-
-    @GetMapping("/")  // 확장자 생략
-    public String home() {
-        return "home"; // view name
-        // 경로 : "/WEB-INF/views/home.html"
-    }
-
-    @GetMapping("/orderSearch")
-    public String orderSearch() {
-        return "order/orderSearch"; // view name (ex: "/WEB-INF/views/order.html")
-    }
-    @GetMapping("/order")
-    public String order() {
-        return "order"; // view name (ex: "/WEB-INF/views/order.html")
-    }
-    @GetMapping("/ordercreate")
-    public String ordercreate() {
-        return "order/ordercreate"; // view name (ex: "/WEB-INF/views/order.html")
-    }
-    @GetMapping("/orderInfo")
-    public String orderInfo() {
-        return "order/orderInfo"; // view name (ex: "/WEB-INF/views/order.html")
-    }
-    }
-
-
-
-
-
-
-
+//package com.example.demo.order.controller;
+//
+////POJO : Plain Old Java Object
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class orderController {
+//
+//
+//    @GetMapping("/")  // 확장자 생략
+//    public String home() {
+//        return "home"; // view name
+//        // 경로 : "/WEB-INF/views/home.html"
+////    }
+//
+//    @GetMapping("/orderSearch")
+//    public String orderSearch() {
+//        return "order/orderSearch"; // view name (ex: "/WEB-INF/views/order.html")
+//    }
+//    @GetMapping("/order")
+//    public String order() {
+//        return "order"; // view name (ex: "/WEB-INF/views/order.html")
+//    }
+//    @GetMapping("/ordercreate")
+//    public String ordercreate() {
+//        return "order/ordercreate"; // view name (ex: "/WEB-INF/views/order.html")
+//    }
+//    @GetMapping("/orderInfo")
+//    public String orderInfo() {
+//        return "order/orderInfo"; // view name (ex: "/WEB-INF/views/order.html")
+//    }
+//    }
+//
+//
+//
+//
+//
+//
+//
